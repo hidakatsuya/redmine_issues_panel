37a67d0951e1f1a88ac35ac8b3a792dfcaecffe5
Katsuya HIDAKA <hidakatsuya@gmail.com>
2024-12-25 11:54:05 +0900

    Fix test failure in IssuesPanelControllerTest#test_index_with_issues_panel_query on Redmine trunk

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,7 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures', %i[issue_queries])
+ActiveSupport::TestCase.fixture_paths << File.dirname(__FILE__) + '/fixtures'
+
+class ActiveSupport::TestCase
+  fixtures :all
+end
